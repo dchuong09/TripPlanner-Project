867efb3647da60e0b363cdbb45bb138690e901fd
dchuong09 <dchuong09@yahoo.com>
2018-04-03 13:54:01 -0700

    completed basic set up

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.use('/', function(req, res) {
+	res.send('Server is running');
+})
 
 app.listen(process.env.PORT || 3000, function() {
 	console.log('Listening on port 3000');
