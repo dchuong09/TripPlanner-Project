149366f4af1fefe2eeba9375b2aa4d03c2cc7930
dchuong09 <dchuong09@yahoo.com>
2018-04-03 14:35:59 -0700

    navBar

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use('/', function(req, res) {
-	res.send('Server is running');
-})
+	res.sendFile('views/index.html', { root: __dirname });
+});
 
 app.listen(process.env.PORT || 3000, function() {
 	console.log('Listening on port 3000');
