7d547315edfd6f077e4f0f9ea9913dd9e760bf92
dchuong09 <dchuong09@yahoo.com>
2018-04-04 11:44:39 -0700

    add jquery move top

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,20 +8,13 @@ console.log('Sanity check!');
 $(document).ready(function() {
 	console.log('app.js loaded')
 
-    $('input[chkboxes]').bind('click', function() {
-        if($(this).is(":checked")) {
-            alert('Hello')
-        } 
-    });
-
-
-
 
 	$.ajax({
 		method: 'GET',
 		url: '/api/experience',
 		success: handleSuccess,
-		error: handleError
+		error: handleError,
+		complete: handleComplete
 	})
 });
 
@@ -35,27 +28,45 @@ function handleError(err) {
 	console.log('Throwing error: ', err);
 };
 
+// When item was clicked, move top
+function handleComplete() {
+
+    $(function(){
+	  var checkedbox;
+	  $('input[type="checkbox"]').on('change', function(){
+
+	    if($(this).prop('checked', 'checked')){
+	      checkedbox = $(this).parent().clone();
+	    }
+	    $(this).parent().remove();
+	    $('.mainList').prepend(checkedbox);
+	  })
+	})
+}
+
+
 function render(experience) {
 	let expHtml = `
-	<div class="container py-3" id="chkboxes">
+	<div class="container py-3">
 		<input type="checkbox">
 		<div class="card">
 		  <div class="row ">
-		    <div class="col-md-4">
-		        <img src="../images/800x800.png" class="w-50">
+		    <div class="col-4">
+		        <img class="w-74" src=${ experience.photo }>
 		      </div>
-		      <div class="col-md-8 px-3">
+		      <div class="col-lg-4 px-3">
 		        <div class="card-block px-3">
 		          <h4 class="card-title">${ experience.name }</h4>
 		          <p class="card-text">${ experience.address }</p>
 		          <p class="card-text1">${ experience.description }</p>
-		          <a href="#" class="btn btn-primary">Read More</a>
+		          <a href="details.html" class="btn btn-primary">Read More</a>
 		        </div>
 		      </div>
 		    </div>
 		  </div>
 	</div>
 	`
+	console.log(experience.photo)
 	$('.mainList').prepend(expHtml);
 
 
