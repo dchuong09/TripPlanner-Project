f03c912cc3d06daba724d040f4ffe93412c8ac7a
dchuong09 <dchuong09@yahoo.com>
2018-04-04 09:06:17 -0700

    fixed header

--- seed.js
+++ seed.js
@@ -4,8 +4,38 @@ const experienceLists = [{
 	name: 'Pier 39',
 	address: 'Fishermans Wharf, North Beach/Telegraph Hill',
 	description: 'Shopping Centers',
-	photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSB8hug_ay5fWl0ebfbrvfmPo88hVxTEZBTU0QY98izEWhDvizo9A',
+	photo: '',
 	duration: '2 hour'
+}, {
+	name: 'Golden Gate Bridge',
+	address: 'Highway 101',
+	description: 'Landmarks & Historical Buildings',
+	photo: '',
+	duration: '2 hour'
+}, {
+	name: 'Twin Peaks',
+	address: '501 Twin Peaks Blvd',
+	description: 'Parks, Landmarks & Historical Buildings',
+	photo: '',
+	duration: '30 minutes'
+}, {
+	name: 'Dolores Park',
+	address: '19th & Dolores St',
+	description: 'Parks, Playgrounds, Basketball Courts',
+	photo: '',
+	duration: '1 hour'	
+}, {
+	name: 'Palace Of Fine Arts',
+	address: '3601 Lyon St',
+	description: 'Landmarks & Historical Buildings, Venues & Event Spaces',
+	photo: '',
+	duration: '1 hour'	
+}, {
+	name: 'Union Square',
+	address: '333 Post St',
+	description: 'Shopping Centers',
+	photo: '',
+	duration: '1 hour'	
 }];
 
 
